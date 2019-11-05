e7833b4c7a52c9bcb30917a4877db726d4b42c03
MyName <e175738@ie.u-ryukyu.ac.jp>
2019-11-05 17:07:04 +0900

    wk4-2

---
+++ regression.py
@@ -0,0 +1,10 @@
+import numpy as np
+class LinearRegression:
+    x = None
+    theta = None
+    y = None
+    def fit(self, x, y): pass
+
+    def predict(self, x): pass
+
+    def score(self, x, y): pass
