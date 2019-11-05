20bf1e8d53cf1afcd37cdbff8d72b1c7673c0d27
MyName <e175738@ie.u-ryukyu.ac.jp>
2019-11-05 17:22:54 +0900

    wk4-3

--- regression.py
+++ regression.py
@@ -3,8 +3,13 @@ class LinearRegression:
     x = None
     theta = None
     y = None
-    def fit(self, x, y): pass
+    def fit(self, x, y):
+        temp = np.linalg.inv(np.dot(x.T, x))
+        self.theta = np.dot(np.dot(temp, x.T), y)
 
-    def predict(self, x): pass
+    def predict(self, x):
+        pass
 
-    def score(self, x, y): pass
+    def score(self, x, y):
+        pass
+    
